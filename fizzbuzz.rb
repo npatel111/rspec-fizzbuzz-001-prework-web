406fab26cb4c60e1db1ef450b85c6a749bcaf2b9
npatel111 <nitimaheshpatel@gmail.com>
2016-08-29 19:55:59 -0400

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(x)
-  if (x % 3 == 0 && x % 5 == 0)
-    return "FizzBuzz"
-  elsif (x % 3 == 0)
-    return "Fizz"
-  elsif (x % 5 == 0)
-    return "Buzz"
+  if x % 3 == 0 && x % 5 == 0 
+    "FizzBuzz"
+  elsif x % 3 == 0 
+    "Fizz"
+  elsif x % 5 == 0
+    "Buzz"
   else
-    return nil
-  end        
+    nil
+  end
 end
